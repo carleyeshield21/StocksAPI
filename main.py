3165646ed8da0c45d2162110228ae31e0656ee2d
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-06-22 14:57:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_API_KEY = 'M31RIST54Y9K6824'
+NEWS_API_KEY = '39709398305945eb818f723ec48de585'
 
 ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -21,9 +22,9 @@ stock_params = {
 
 response = requests.get(STOCK_ENDPOINT,params=stock_params)
 stocks_data = response.json()
+stocks_data1 = response.json()['Weekly Time Series']
 # print(stocks_data)
 # print('=======')
-stocks_data1 = response.json()['Weekly Time Series']
 # print(stocks_data1)
 # stocks_data_meta = response.json()['Meta Data']
 # print(stocks_data_meta)
@@ -60,17 +61,36 @@ percent_diff = (diff_closing_value / float(last_week_close_value)) * 100
 print(percent_diff)
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+## STEP 2: https://newsapi.org/
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+if percent_diff > 0.3:
+    news_params = {
+        'apiKey': NEWS_API_KEY,
+        'qInTitle': COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT,params=news_params)
+    print(news_response.json()['articles'][:3])
+    # print(news_response.json())
+    # print('====')
+    # print(news_response.json()['articles'])
+    # print('====')
+    # print(type(news_response.json()))
+    # print(len(news_response.json()))
+    # print(news_response.json()['articles'][0])
+    # print('====')
+    # print(news_response.json()['articles'][1])
+    # print('====')
+    # print(news_response.json()['articles'][2])
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+## STEP 3: Use twilio.com/docs/sms/quickstart/python
+#to send a separate message with each article's title and description to your phone number.
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
@@ -88,4 +108,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
